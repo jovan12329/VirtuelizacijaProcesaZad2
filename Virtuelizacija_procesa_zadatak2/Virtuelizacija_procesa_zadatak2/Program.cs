c1653466199e2ee81952284a14d7c6724270826d
jovan12329 <116072716+jovan12329@users.noreply.github.com>
2023-05-27 12:10:02 +0200

    Starting template

---
+++ Virtuelizacija_procesa_zadatak2/Virtuelizacija_procesa_zadatak2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtuelizacija_procesa_zadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
